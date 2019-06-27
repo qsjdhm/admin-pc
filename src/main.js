6a9ddb437aa6a43df231dc666b55d30d349dcf3b
DESKTOP-TR0K2SP\admin <zhangyan1@vtstar.net>
2019-06-27 13:50:24 +0800

    init：初始化项目

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import './plugins/element.js'
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+new Vue({
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
